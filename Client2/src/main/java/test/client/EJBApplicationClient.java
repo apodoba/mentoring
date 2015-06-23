4295ab907fd3779d85f5ac97cffb84e69d67216e
arina <arinapodoba@gmail.com>
2015-06-23 22:35:19 +0300

    Client refactoring

--- Client2/src/main/java/test/client/EJBApplicationClient.java
+++ Client2/src/main/java/test/client/EJBApplicationClient.java
@@ -1,32 +1,40 @@
 package test.client;
 
+import java.math.BigDecimal;
 import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
-import com.apodoba.shop.beans.Purchase;
-import com.apodoba.shop.domain.User;
+import com.apodoba.shop.beans.PurchaseBean;
+import com.apodoba.shop.domain.bank.Card;
+import com.apodoba.shop.domain.shop.User;
 
 public class EJBApplicationClient {
 
 	public static void main(String[] args) {
 		try {
 			Properties env = new Properties();
-			env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+			env.put(Context.INITIAL_CONTEXT_FACTORY,
+					"org.jboss.naming.remote.client.InitialContextFactory");
 			env.put(Context.PROVIDER_URL, "remote://localhost:4447");
 			env.put("jboss.naming.client.ejb.context", true);
 
 			Context ctx = new InitialContext(env);
-			Purchase hello = (Purchase) ctx.lookup("Shop/PurchaseBean!com.apodoba.shop.beans.Purchase");
+			PurchaseBean transactionalService = (PurchaseBean) ctx
+					.lookup("Shop/PurchaseBeanImpl!com.apodoba.shop.beans.PurchaseBean");
 
 			User user = new User();
 			user.setFirstName("Firstname");
 			user.setLastName("Lastname");
-			user.setLogin("login");
 			user.setPassword("password");
 			user.setMail("mail.ru");
-			hello.saveUser(user);
+
+			Card card = new Card();
+			card.setCount(new BigDecimal(10));
+			card.setCvv(987);
+			card.setEndYear(2016);
+			card.setNumber(8909765456782341l);
 
 			System.out.println("EJB says: saved");
 		} catch (Exception e) {
