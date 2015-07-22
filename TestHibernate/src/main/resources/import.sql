545dbe119e7a90428325d833b17797a9f41c5b5a
Arina Podoba <arinapodoba@gmail.com>
2015-07-22 16:07:13 +0300

    Change name

--- TestHibernate/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
--- You can use this file to load seed data into the database using SQL statements
-insert into Project (id, name, description) values (0, 'Kinnarps', 'project');
-insert into Unit (id, name, description) values (0, 'Unit', 'unit description');
